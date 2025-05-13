39ab602cc2643fdee90ec127a8dfceac00c0d06f
Tarverdiyeva Aysel <t.aysel2017@gmail.com>
2025-05-13 19:48:35 +0400

    commit

--- vite-project/src/pages/News/WeatherNews.jsx
+++ vite-project/src/pages/News/WeatherNews.jsx
@@ -5,115 +5,114 @@ import css from './WeatherNews.module.css';
 
 export default function WeatherNews() {
 
-    const [search, setSearch] = useState("");
-    const [result, setResult] = useState("Baku");
-    const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState("Baku");
+  const [data, setData] = useState([]);
 
-    const handleSearch = () => {
-        setResult(search);
-    };
+  const handleSearch = () => {
+    setResult(search);
+  };
 
-    const newsApi = "307847ce4f4f4c49bce05662a192bcfd";
-    const url = `https://newsapi.org/v2/everything?q=weather&apiKey=${newsApi}`;  
-    useEffect(() => {
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'ok') {
-                const articles = data.articles;
-    
-                setData(
-                    data.articles.slice(0, 6).map(article => ({
-                      title: article.title,
-                      source: article.source.name,
-                      url: article.url,
-                      publishedAt: article.publishedAt,
-                      image: article.urlToImage,
-                      description: article.description
-                    }))
-                );
-            }   else {
-                console.error('Xəbər alınmadı:', data.message);
-            }
-        })
-    }, [search])
+  const newsApi = "307847ce4f4f4c49bce05662a192bcfd";
+  const url = `https://newsapi.org/v2/everything?q=weather&apiKey=${newsApi}`;
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'ok') {
+          const articles = data.articles;
 
-    const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        const date = new Date(dateString);
-        return date.toLocaleDateString('en-GB', options);
-    };
+          setData(
+            data.articles.slice(0, 6).map(article => ({
+              title: article.title,
+              source: article.source.name,
+              url: article.url,
+              publishedAt: article.publishedAt,
+              image: article.urlToImage,
+              description: article.description
+            }))
+          );
+        } else {
+          console.error('Xəbər alınmadı:', data.message);
+        }
+      })
+  }, [search])
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-GB', options);
+  };
 
   return (
     <div className={css.container}>
-        <div className={css.navbar}>
-            <Navbar2 
-                search={search}
-                setSearch={setSearch}
-                onSearch={handleSearch}
-                currentWeather={{ city: 'Baku', country: 'Azerbaijan' }} 
-            />
-        </div>
-        <div className={css.sidebar}>
-            <Sidebar/>
-        </div>
-        <div className={css.content}>
-            <div className={css.scrollableContent}>
-                <div className={css.weatherNews}>
-                    <div className={css.up}>
-                        {data.slice(0, 3).map((article, index) => (
-                          <div key={index} className={css.news}>
-                            <div className={css.firstPart}>
-                              <div>
-                                <img className={css.img} src={article.image} alt={article.title} />
-                              </div>
-                              <div>
-                                <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
-                              </div>
-                            </div>
-                            <div className={css.lastPart}>
-                              <div className={css.date}>
-                                <img className={css.icon} src="https://i.postimg.cc/KvWWdtFK/calendar.png" alt="Calendar" />
-                                {/* <p>{article.publishedAt}</p> */}
-                                <p>{formatDate(article.publishedAt)}</p>
-                              </div>
-                              <a href={article.url} target="_blank" rel="noopener noreferrer">
-                                <button className={css.button}>
-                                    Read More <img className={css.icon} src="https://i.postimg.cc/bJzh3ZRg/next.png" alt="Next" />
-                                </button>
-                              </a>
-                            </div>
-                          </div>
-                        ))}
+      <div className={css.navbar}>
+        <Navbar2
+          search={search}
+          setSearch={setSearch}
+          onSearch={handleSearch}
+          currentWeather={{ city: 'Baku', country: 'Azerbaijan' }}
+        />
+      </div>
+      <div className={css.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={css.content}>
+        <div className={css.scrollableContent}>
+          <div className={css.weatherNews}>
+            <div className={css.up}>
+              {data.slice(0, 3).map((article, index) => (
+                <div key={index} className={css.news}>
+                  <div className={css.firstPart}>
+                    <div>
+                      <img className={css.img} src={article.image} alt={article.title} />
                     </div>
-                    <div className={css.down}>
-                      {data.slice(3, 6).map((article, index) => (
-                        <div key={index} className={css.news}>
-                          <div className={css.firstPart}>
-                            <div>
-                              <img className={css.img} src={article.image} alt={article.title} />
-                            </div>
-                            <div>
-                              <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
-                            </div>
-                          </div>
-                          <div className={css.lastPart}>
-                            <div className={css.date}>
-                              <img className={css.icon} src="https://i.postimg.cc/KvWWdtFK/calendar.png" alt="Calendar" />
-                              <p>{formatDate(article.publishedAt)}</p>
-                            </div>
-                            <a href={article.url} target="_blank" rel="noopener noreferrer">
-                                <button className={css.button}>
-                                Read More <img className={css.icon} src="https://i.postimg.cc/bJzh3ZRg/next.png" alt="Next" />
-                                </button>
-                            </a>
-                          </div>
-                        </div>
-                      ))}
+                    <div>
+                      <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
                     </div>
+                  </div>
+                  <div className={css.lastPart}>
+                    <div className={css.date}>
+                      <img className={css.icon} src="https://i.postimg.cc/KvWWdtFK/calendar.png" alt="Calendar" />
+                      <p>{formatDate(article.publishedAt)}</p>
+                    </div>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer">
+                      <button className={css.button}>
+                        Read More <img className={css.icon} src="https://i.postimg.cc/bJzh3ZRg/next.png" alt="Next" />
+                      </button>
+                    </a>
+                  </div>
                 </div>
+              ))}
             </div>
+            <div className={css.down}>
+              {data.slice(3, 6).map((article, index) => (
+                <div key={index} className={css.news}>
+                  <div className={css.firstPart}>
+                    <div>
+                      <img className={css.img} src={article.image} alt={article.title} />
+                    </div>
+                    <div>
+                      <a href={article.url} target="_blank" rel="noopener noreferrer">{article.title}</a>
+                    </div>
+                  </div>
+                  <div className={css.lastPart}>
+                    <div className={css.date}>
+                      <img className={css.icon} src="https://i.postimg.cc/KvWWdtFK/calendar.png" alt="Calendar" />
+                      <p>{formatDate(article.publishedAt)}</p>
+                    </div>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer">
+                      <button className={css.button}>
+                        Read More <img className={css.icon} src="https://i.postimg.cc/bJzh3ZRg/next.png" alt="Next" />
+                      </button>
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
